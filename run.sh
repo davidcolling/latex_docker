111fb666d9d482f44e8a1063867adcfab7ec5042
d <d@365922.local>
2021-06-27 15:34:17 -0500

    depersonalized

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
-cp $1 /Users/d/documents/art/code/environment/docker/latex/cpbox/input.tex
-cp ./*.sty /Users/d/documents/art/code/environment/docker/latex/cpbox/
-cp ./*.cls /Users/d/documents/art/code/environment/docker/latex/cpbox/
-cp ./*.png /Users/d/documents/art/code/environment/docker/latex/cpbox/
-cp ./*.jpg /Users/d/documents/art/code/environment/docker/latex/cpbox/
-cp ./*.jpeg /Users/d/documents/art/code/environment/docker/latex/cpbox/
-docker run -it --rm --mount type=bind,src="/Users/d/Documents/art/code/environment/docker/latex/cpbox",dst="/cpbox" latex bash -c "cd /cpbox; pdflatex input.tex"
-cp /Users/d/documents/art/code/environment/docker/latex/cpbox/input.pdf ./output.pdf
-rm -rf ~/documents/art/code/environment/docker/latex/cpbox/*
+cp $1 /Users/d/documents/art/code/environment/docker/latex_docker/cpbox/input.tex
+cp ./*.sty /Users/d/documents/art/code/environment/docker/latex_docker/cpbox/
+cp ./*.cls /Users/d/documents/art/code/environment/docker/latex_docker/cpbox/
+cp ./*.png /Users/d/documents/art/code/environment/docker/latex_docker/cpbox/
+cp ./*.jpg /Users/d/documents/art/code/environment/docker/latex_docker/cpbox/
+cp ./*.jpeg /Users/d/documents/art/code/environment/docker/latex_docker/cpbox/
+docker run -it --rm --mount type=bind,src="$DOCKER/latex_docker/cpbox",dst="/cpbox" latex bash -c "cd /cpbox; pdflatex input.tex"
+cp /Users/d/documents/art/code/environment/docker/latex_docker/cpbox/input.pdf ./output.pdf
+rm -rf ~/documents/art/code/environment/docker/latex_docker/cpbox/*
